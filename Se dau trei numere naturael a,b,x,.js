bea3fe25d61dfbb279875e21787a26643b98cc1c
Sergiu Chis <leytah@yahoo.com>
2017-11-21 18:10:04 +0200

    x Apartine intervalului a,b

---
+++ Se dau trei numere naturael a,b,x,.js
@@ -0,0 +1,9 @@
+//Se dau trei numere naturale a b x. Să se verifice dacă numărul x aparține intervalului [a,b].
+
+function xApartineIntervalului(a,b,x){
+	if (a<=x&&x<=b) {
+		return true
+	}else{
+		return false
+	}
+}
