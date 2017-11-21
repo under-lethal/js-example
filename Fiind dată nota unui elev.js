85f8622a13fb02a8f6b6a31954ce2e9af0d56b5e
Sergiu Chis <leytah@yahoo.com>
2017-11-21 16:46:28 +0200

    corigent/promovat

---
+++ Fiind dată nota unui elev.js
@@ -0,0 +1,10 @@
+//Fiind dată nota unui elev să se afișeze dacă acesta este corigent sau promovat.
+
+function corigentPromovat (c) {
+	if (c>=5){
+	return "promovat"
+	}
+	else {
+	return "corigent"
+	}
+}
