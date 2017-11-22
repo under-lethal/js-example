4dc127f33f493300f1669592245517a0c0d14884
Sergiu Chis <leytah@yahoo.com>
2017-11-22 23:30:47 +0200

    det.minimul a 3 numere.

---
+++ Minimul a 3 numere.js
@@ -0,0 +1,13 @@
+// Sa se scrie un program care determina minimul a trei numere intregi.
+
+function getMinNumber(a,b,c) {
+	if (a<b) {
+	return a
+	}
+	else{
+	return b
+	}
+	function getMinNumber2(a,b,c){
+	return getMin (a,b), getMin(b,c)
+	}
+}
