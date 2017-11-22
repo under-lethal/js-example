43a74b6a3acc14612ce0e8e96a05c4925ef1d4cb
Sergiu Chis <leytah@yahoo.com>
2017-11-23 00:28:38 +0200

    printRange2Numbers

---
+++ Functie care primeste doi parametrii. Print range.js
@@ -0,0 +1,8 @@
+//Functie care primeste doi parametrii. Primul e mai mic decat al doilea. Functia trebuie sa printeze toate numerele de la primul la al doilea. (implicit pe primul si al doilea).
+
+function printNumberRange(x,y){
+	var i = x;
+	while (i<=y){
+	console.log (i)
+	i=i+1;}
+}
