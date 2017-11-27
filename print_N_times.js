aa4522cf1b1e0b34c7935f1192832fdfe759d10a
Sergiu Chis <leytah@yahoo.com>
2017-11-28 00:06:12 +0200

    print "a" how many times "x" said.

---
+++ print_N_times.js
@@ -0,0 +1,9 @@
+// Functie care sa ia 2 parametrii (numere naturale). Printeaza primul parametru de cate ori zice al doilea.
+
+	function printNTimes (a,x) {
+		var i = 0
+		while (i<x){
+			console.log (a)
+			i=i+1;
+		}
+	}
