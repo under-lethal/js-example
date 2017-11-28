e12d51f641014aa8cd98edb08f7621bc33ae0fc7
under-lethal <leytah@yahoo.com>
2017-11-28 16:14:02 +0200

    Update print_three_times.js

--- print_three_times.js
+++ print_three_times.js
@@ -1,9 +1,9 @@
 // Functie care ia un parametru si il printeaza de 3x.
 
 function prtintThreeTimes(x){
-var i = 0
-while (i<3){
-console.log (x)
-i++
-}
+  var i = 0
+  while (i<3){
+    console.log (x)
+   i++
+  }
 };
